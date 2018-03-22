d236e742811d7a7143c1ad84286e5b9ac615ef2a
David Germain <davidispizza@gmail.com>
2018-03-21 20:23:07 -0400

    Make the desired interface

--- source/index.js
+++ source/index.js
@@ -3,10 +3,15 @@ import Api from './chickadee-api';
 import express from 'express';
 
 const app = express();
-const api = Api.create('http://euclid.nmu.edu:11223/api', true);
+const port = 3000;
 
-const frequency = 60000;
+const api = new Api('http://euclid.nmu.edu:11223/api');
+
 let total = 0;
+
+api.on('visit', () => {
+  total++;
+});
 
 app.get('/total', (req, res) => {
   res.json({
@@ -15,23 +20,5 @@ app.get('/total', (req, res) => {
   res.end();
 });
 
-count().then((c) => {
-  total = c;
-});
-
-setInterval(() => {
-  count().then((c) => {
-    total = c;
-  });
-}, frequency);
-
-function count() {
-  return api.get('visits?start=0&end=100000000000')
-    .then((visits) => {
-      return visits.length;
-    });
-}
-
-const port = 3000;
 console.log(`port ${port}`);
 app.listen(port);
