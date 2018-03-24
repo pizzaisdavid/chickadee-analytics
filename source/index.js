e1bdf87b11e0315eeb8784c28c4524fb93b4ba73
David Germain <davidispizza@gmail.com>
2018-03-24 00:20:59 -0400

    Allow CORS

--- source/index.js
+++ source/index.js
@@ -18,8 +18,16 @@ api.on('initialize', () => {
 api.on('visit', () => {
   total++;
 });
+// Add headers
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
-app.get('/total', (req, res) => {
+app.get('/api/total', (req, res) => {
   res.json({
     'total': total
   });
