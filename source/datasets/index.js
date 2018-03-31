f7b266ca3a4ef36941f77b1961936db3ce0ed179
David Germain <davidispizza@gmail.com>
2018-03-30 21:57:51 -0400

    add test

--- source/datasets/index.js
+++ source/datasets/index.js
@@ -1,8 +1,10 @@
 
 import empty from './empty';
 import single from './single';
+import simple from './simple';
 
 export default [
   empty,
   single,
+  simple,
 ];
--- source/datasets/simple.js
+++ source/datasets/simple.js
@@ -2,7 +2,7 @@
 import { RESOURCES } from '../Statistics';
 
 export default {
-  name: 'single',
+  name: 'simple',
   clock: { timestamp: 60 },
   config: {
     [RESOURCES.RECENT_VISITS_SUMMARY]: {
@@ -10,25 +10,32 @@ export default {
       grouping: 10,
     },
   },
-  birds: [{ id: 'a' }],
-  feeders: [{ id: 'Z', longitude: 0, latitude: 0 }],
+  birds: [
+    { id: 'a' },
+    { id: 'b' },
+  ],
+  feeders: [
+    { id: 'Z', longitude: 0, latitude: 0 },
+  ],
   visits: [
+    { timestamp: 50, feeder: 'Z', bird: 'a'},
     { timestamp: 55, feeder: 'Z', bird: 'a'},
+    { timestamp: 50, feeder: 'Z', bird: 'b'},
+    { timestamp: 55, feeder: 'Z', bird: 'b'},
   ],
   statistics: {
     visits: {
-      total: 1,
+      total: 4,
       grouped: {
         30: 0,
         40: 0,
-        50: 1,
+        50: 4,
       },
     },
     birds: {
       checkins: {
-        a: {
-          Z: 1,
-        },
+        a: { Z: 2 },
+        b: { Z: 2 },
       },
     },
   },
